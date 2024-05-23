66822d14976fcc550283cbdb54b2dade85a224b0
Ariff Sayyad <125879041+ariffsayyad@users.noreply.github.com>
2024-05-23 15:10:38 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,19 +190,19 @@ document.onkeydown = function (e) {
     }
 }
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/664f07fd981b6c5647738889/1huibee9u';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/664f07fd981b6c5647738889/1huibee9u';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+// </script>
+// <!--End of Tawk.to Script-->
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
