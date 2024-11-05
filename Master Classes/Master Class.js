4c986f573e11ffc36775541d2ed1f260cf6b6b50
Ariff Sayyad <125879041+ariffsayyad@users.noreply.github.com>
2024-11-06 02:50:28 +0530

    Add files via upload

---
+++ Master Classes/Master Class.js
@@ -0,0 +1,14 @@
+function toggleClick(button) {
+  button.classList.toggle("clicked");
+}
+
+
+function downloadPDF_1st_Card() {
+  const pdfUrl = "https://drive.google.com/file/d/10wjz8tUUN8ZEaqyvYxAMtm6rDQpMPXwP/view?usp=sharing";
+  window.open(pdfUrl, "_blank");
+}
+function downloadPDF_2st_Card() {
+  const pdfUrl = "https://drive.google.com/file/d/1CTP2ZXqApq-wyCyCP8OBRlFGumoQRFUM/view?usp=drive_link";
+  window.open(pdfUrl, "_blank");
+}
+
