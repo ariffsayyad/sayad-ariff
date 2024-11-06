8c42638bbf411875834869f7993bfd33c15a3ee4
Ariff Sayyad <125879041+ariffsayyad@users.noreply.github.com>
2024-11-07 00:31:14 +0530

    Update Master Class.js

--- Master Classes/Master Class.js
+++ Master Classes/Master Class.js
@@ -13,30 +13,30 @@ function downloadPDF_2st_Card() {
 }
 
 $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
+  $(this).toggleClass('fa-times');
+  $('.navbar').toggleClass('nav-toggle');
+});
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
+$(window).on('scroll load', function () {
+  $('#menu').removeClass('fa-times');
+  $('.navbar').removeClass('nav-toggle');
 
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
+  if (window.scrollY > 60) {
+      document.querySelector('#scroll-top').classList.add('active');
+  } else {
+      document.querySelector('#scroll-top').classList.remove('active');
+  }
 
-        // scroll spy
-        $('section').each(function () {
-            let height = $(this).height();
-            let offset = $(this).offset().top - 200;
-            let top = $(window).scrollTop();
-            let id = $(this).attr('id');
+  // scroll spy
+  $('section').each(function () {
+      let height = $(this).height();
+      let offset = $(this).offset().top - 200;
+      let top = $(window).scrollTop();
+      let id = $(this).attr('id');
 
-            if (top > offset && top < offset + height) {
-                $('.navbar ul li a').removeClass('active');
-                $('.navbar').find(`[href="#${id}"]`).addClass('active');
-            }
-        });
-    });
+      if (top > offset && top < offset + height) {
+          $('.navbar ul li a').removeClass('active');
+          $('.navbar').find(`[href="#${id}"]`).addClass('active');
+      }
+  });
+});
